c360f8b92d7d20c88e6c99c0c7433cd01c962211
FatyBp <fatimabenitez796@gmail.com>
2023-09-22 13:25:27 -0600

    cambio

--- src/components/Footer/Footera.jsx
+++ src/components/Footer/Footera.jsx
@@ -1,10 +1,10 @@
 export const Footera = ({porcentaje}) => {
   return (
     <>
-    <div className="progress">
+  <div className="progress">
         <div className="progress-bar bg-success progress-bar-animated" 
         role="progressbar" 
-        style={{width: `${porcentaje * 100}%`}}
+        style={{width:`${porcentaje * 100}%`}}
         aria-valuenow={porcentaje * 100} 
         aria-valuemin="0" 
         aria-valuemax="100">
